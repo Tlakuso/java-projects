b58bb771161e2bf53f2da8de767c2e2dc9547ae4
Tlakuso Matshabele <138527697+Tlakuso@users.noreply.github.com>
2023-11-05 17:37:06 +0200

    Add files via upload

---
+++ calculator_app/src/calculator_app/Calculator_app.java
@@ -0,0 +1,23 @@
+
+package calculator_app;
+
+
+
+
+public class Calculator_app {
+
+    
+    public static void main(String[] args) {
+        /*
+        JPanel panel = new JPanel();
+        Button btn1 = new Button("exit");
+        JFrame frame = new JFrame();
+        frame.setSize(500,750);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        */
+        
+        
+    }
+    
+}
